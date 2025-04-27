d58311aebae2e3843ff784b9c26748226ecd22b9
Leeaj6GitHub <leeaj6@appstate.edu>
2025-04-27 19:59:08 -0400

    Skeleton code for backend implemented

--- webappBackend/venv/app.py
+++ webappBackend/venv/app.py
@@ -1,8 +1,60 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/airplane")
+def airplane():
+    return 501
+
+
+@app.route("/automobile")
+def automobile():
+    return 501
+
+
+@app.route("/bird")
+def bird():
+    return 501
+
+
+@app.route("/cat")
+def cat():
+    return 501
+
+
+@app.route("/deer")
+def deer():
+    return 501
+
+@app.route("/dog")
+def dog():
+    return 501
+
+
+@app.route("/frog")
+def frog():
+    return 501
+
+
+@app.route("/horse")
+def horse():
+    return 501
+
+
+@app.route("/ship")
+def ship():
+    return 501
+
+
+@app.route("/truck")
+def truck():
+    return 501
+
 
